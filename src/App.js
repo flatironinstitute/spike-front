9f522a1bb5b1db4ae7dbea0772e8f9819e762873
Elizabeth Lovero <elovero@flatironinstitute.org>
2018-10-31 15:46:13 -0400

    Basic fonts and container styling.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Spikeforest</p>
+          <h1>SpikeForest</h1>
+          <p>
+            Benchmarking of spike sorting algorithms. Seeing the spike forest
+            for the trees.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut
+            maximus eros. Aliquam pretium pretium tincidunt. Nam ornare leo a
+            tincidunt facilisis. Nullam rutrum sagittis odio, at pulvinar sapien
+            elementum et.
+          </p>
           <a
             className="App-link"
             href="https://www.simonsfoundation.org/flatiron/"
