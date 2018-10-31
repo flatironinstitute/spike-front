d9421097f09c21f6c79880f4a7c56b41bcf85550
Elizabeth Lovero <elovero@flatironinstitute.org>
2018-10-31 13:56:08 -0400

    Add normalize and add css variables.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Spikeforest</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.simonsfoundation.org/flatiron/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn More
           </a>
         </header>
       </div>
