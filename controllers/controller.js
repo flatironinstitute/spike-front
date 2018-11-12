e11ce1d41527e38fcb36ed3d014648122756a334
Elizabeth Lovero <elovero@flatironinstitute.org>
2018-11-12 14:35:42 -0500

    Update list.

--- controllers/controller.js
+++ controllers/controller.js
@@ -44,6 +44,6 @@ exports.getBatchResults = async (req, res, next) => {
 // 3. Group the sorting results by study name and note the sorter name ✅
 // 4. Make an object for each study with sub-objects for each sorter and sub-sub objects for each recording ✅
 // 5. Convert the comparison with truth on each recording into a http url ✅
-// 6. Make an http request to get each comparison with truth
-// 7. Get the accuracy of all the firings that were sorted from this JSON and make into an array.
-// 8. Add this array to the study object under the storer and recording.
+// 6. Make an http request to get each comparison with truth  ✅
+// 7. Get the accuracy of all the firings that were sorted from this JSON and make into an array. ✅
+// 8. Add this array to the study object under the storer and recording. ✅
