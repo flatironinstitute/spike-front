9e7859c7b10e95708596e0bce7c72963c38e252d
Elizabeth Lovero <elovero@flatironinstitute.org>
2018-11-28 11:09:32 -0500

    Unstyled slider.

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -2,30 +2,23 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 import HeatmapLabelYAxis from "./HeatmapLabelYAxis";
 import HeatmapLabelXAxis from "./HeatmapLabelXAxis";
-import Legend from "./Legend";
 
 class Heatmap extends Component {
   constructor(props) {
     super(props);
-    // TODO: refactor out and replace colors.
-    this.state = {
-      colors: [
-        "#ffffd9",
-        "#edf8b1",
-        "#c7e9b4",
-        "#7fcdbb",
-        "#41b6c4",
-        "#1d91c0",
-        "#225ea8",
-        "#253494",
-        "#081d58"
-      ]
-    };
+    this.svg = d3.select("#heatmap-svg");
   }
 
   componentDidMount() {
     const svg = d3.select("#heatmap-svg");
     this.buildGrid(svg, this.props.builtData);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.builtData !== prevProps.builtData) {
+      console.log("🏤 Component did update in heatmap");
+      this.buildGrid(this.svg, this.props.builtData);
+    }
   }
 
   buildGrid(svg, builtData) {
@@ -39,7 +32,7 @@ class Heatmap extends Component {
           return d.in_range;
         })
       ])
-      .range(this.state.colors);
+      .range(this.props.colors);
 
     // Axis translation
     // studies = y
@@ -84,7 +77,7 @@ class Heatmap extends Component {
       .attr("class", "sorter bordered")
       .attr("width", gridSize)
       .attr("height", gridSize)
-      .style("fill", this.state.colors[0]);
+      .style("fill", this.props.colors[0]);
 
     heatMap
       .transition()
@@ -110,68 +103,32 @@ class Heatmap extends Component {
   render() {
     const toTop = this.props.height + 300;
     return (
-      <div className="container container__heatmap--row">
-        <div className="heatmap__legend col--2">
-          <Legend
-            gridSize={this.props.gridSize}
-            colors={this.state.colors}
-            builtData={this.props.builtData}
-            width={this.props.width}
-            height={this.props.height}
-          />
-        </div>
-        <div className="heatmap__col col--6">
-          <g className="heatmap">
-            <svg id="heatmap-svg" />
-            {this.props.studies.map((study, i) => (
-              <HeatmapLabelYAxis
-                key={i * this.props.gridSize + "Y"}
-                x={0}
-                y={i * this.props.gridSize}
-                label={study}
-                translateY={this.getTranslationY(i)}
-                translateX={20}
-                id="heatmap-label__study"
-              />
-            ))}
-            {this.props.sorters.map((sorter, i) => (
-              <HeatmapLabelXAxis
-                key={i * this.props.gridSize + "X"}
-                x={i * this.props.gridSize}
-                y={0}
-                label={sorter}
-                index={i}
-                translateY={toTop * -1}
-                id="heatmap-label__sorter"
-              />
-            ))}
-          </g>
-        </div>
-        {/* TODO: Refactor into a separate component */}
-        <div className="unitdetail col--4">
-          <h4 className="unitdetail__title">Detail View TK</h4>
-          <div className="unitdetail__copy">
-            <ul className="unitdetail__list">
-              <li>firing_rate: 2.33</li>
-              <li>in_range: 80</li>
-              <li>num_events: 1398</li>
-              <li>peak_channel: 0</li>
-              <li>recording: "001_synth"</li>
-              <li>snr: 25.396783859187707</li>
-              <li>sorter: "MountainSort4-thr3"</li>
-              <li>study: "magland_synth_noise10_K10_C4"</li>
-            </ul>
-            <p>
-              Boggarts lavender robes, Hermione Granger Fantastic Beasts and
-              Where to Find Them. Bee in your bonnet Hand of Glory elder wand,
-              spectacles House Cup Bertie Bott’s Every Flavor Beans Impedimenta.
-              Stunning spells tap-dancing spider Slytherin’s Heir mewing kittens
-              Remus Lupin. Palominos scarlet train black robes, Metamorphimagus
-              Niffler dead easy second bedroom. Padma and Parvati Sorting Hat
-              Minister of Magic blue turban remember my last.
-            </p>
-          </div>
-        </div>
+      <div>
+        <g className="heatmap">
+          <svg id="heatmap-svg" />
+          {this.props.studies.map((study, i) => (
+            <HeatmapLabelYAxis
+              key={i * this.props.gridSize + "Y"}
+              x={0}
+              y={i * this.props.gridSize}
+              label={study}
+              translateY={this.getTranslationY(i)}
+              translateX={20}
+              id="heatmap-label__study"
+            />
+          ))}
+          {this.props.sorters.map((sorter, i) => (
+            <HeatmapLabelXAxis
+              key={i * this.props.gridSize + "X"}
+              x={i * this.props.gridSize}
+              y={0}
+              label={sorter}
+              index={i}
+              translateY={toTop * -1}
+              id="heatmap-label__sorter"
+            />
+          ))}
+        </g>
       </div>
     );
   }
--- src/components/HeatmapContainer.js
+++ src/components/HeatmapContainer.js
@@ -2,36 +2,70 @@ import React, { Component } from "react";
 import Heatmap from "./Heatmap";
 import { isEmpty } from "../utils";
 import Preloader from "./Preloader";
+import Legend from "./Legend";
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
 
 class HeatmapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      builtData: []
+      builtData: [],
+      accuracy: 0.8
     };
     // TODO: Make margin and overall height / width responsive
     this.margin = { top: 50, right: 0, bottom: 100, left: 226 };
     this.width = 440;
     this.height = 400 + this.margin.top + this.margin.bottom;
+    this.colors = [
+      "#ffffd9",
+      "#edf8b1",
+      "#c7e9b4",
+      "#7fcdbb",
+      "#41b6c4",
+      "#1d91c0",
+      "#225ea8",
+      "#253494",
+      "#081d58"
+    ];
   }
 
   componentDidMount() {
-    if (this.props.results.length) {
-      this.buildData().then(data => {
-        this.setState({
-          builtData: data
-        });
-      });
+    if (this.props.allUnits.length) {
+      this.filterAccuracy();
     }
   }
 
-  async buildData() {
-    const builtData = await this.formatData();
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.accuracy !== prevState.accuracy) {
+      console.log("🏤 Component did update");
+      this.filterAccuracy();
+    }
+  }
+
+  filterAccuracy() {
+    let filtered = this.props.results.map(result => {
+      let above = result.accuracies.filter(accu => {
+        return accu >= this.state.accuracy;
+      });
+      console.log("🇪🇺", above.length, result.in_range);
+      result.in_range = above.length;
+      return result;
+    });
+    this.buildData(filtered).then(data => {
+      this.setState({
+        builtData: data
+      });
+    });
+  }
+
+  async buildData(filtered) {
+    const builtData = await this.formatData(filtered);
     return builtData;
   }
 
-  formatData() {
-    return this.props.results.map(result => {
+  formatData(filtered) {
+    return filtered.map(result => {
       return {
         study: result.study,
         in_range: result.in_range,
@@ -40,25 +74,89 @@ class HeatmapContainer extends Component {
     });
   }
 
+  handleAccuracyChangeStart = () => {
+    console.log("🏰 Change event started");
+  };
+
+  handleAccuracyChange = value => {
+    this.setState({
+      accuracy: value
+    });
+  };
+
+  handleChangeComplete = () => {
+    console.log("📪 Change event completed");
+  };
+
   render() {
     let loading = isEmpty(this.state.builtData);
     // TODO: Make grid size responsive.
     let gridSize = Math.floor(this.height / this.props.studies.length);
+    let value = this.state.accuracy;
     return (
       <div>
         {loading ? (
           <Preloader />
         ) : (
-          <Heatmap
-            builtData={this.state.builtData}
-            sorters={this.props.sorters}
-            studies={this.props.studies}
-            gridSize={gridSize}
-            margin={this.margin}
-            width={this.width}
-            height={this.height}
-            allUnits={this.props.allUnits}
-          />
+          <div className="container container__heatmap--row">
+            <div className="heatmap__legend col--2">
+              <Legend
+                gridSize={gridSize}
+                colors={this.colors}
+                builtData={this.state.builtData}
+                width={this.width}
+                height={this.height}
+              />
+              <Slider
+                min={0}
+                max={1}
+                value={value}
+                step={0.01}
+                onChangeStart={this.handleAccuracyChangeStart}
+                onChange={this.handleAccuracyChange}
+                onChangeComplete={this.handleAccuracyChangeComplete}
+              />
+            </div>
+            <div className="heatmap__col col--6">
+              <Heatmap
+                colors={this.colors}
+                builtData={this.state.builtData}
+                sorters={this.props.sorters}
+                studies={this.props.studies}
+                gridSize={gridSize}
+                margin={this.margin}
+                width={this.width}
+                height={this.height}
+                allUnits={this.props.allUnits}
+              />
+            </div>
+            {/* TODO: Refactor into a separate component */}
+            <div className="unitdetail col--4">
+              <h4 className="unitdetail__title">Detail View TK</h4>
+              <div className="unitdetail__copy">
+                <ul className="unitdetail__list">
+                  <li>firing_rate: 2.33</li>
+                  <li>in_range: 80</li>
+                  <li>num_events: 1398</li>
+                  <li>peak_channel: 0</li>
+                  <li>recording: "001_synth"</li>
+                  <li>snr: 25.396783859187707</li>
+                  <li>sorter: "MountainSort4-thr3"</li>
+                  <li>study: "magland_synth_noise10_K10_C4"</li>
+                </ul>
+                <p>
+                  Boggarts lavender robes, Hermione Granger Fantastic Beasts and
+                  Where to Find Them. Bee in your bonnet Hand of Glory elder
+                  wand, spectacles House Cup Bertie Bott’s Every Flavor Beans
+                  Impedimenta. Stunning spells tap-dancing spider Slytherin’s
+                  Heir mewing kittens Remus Lupin. Palominos scarlet train black
+                  robes, Metamorphimagus Niffler dead easy second bedroom. Padma
+                  and Parvati Sorting Hat Minister of Magic blue turban remember
+                  my last.
+                </p>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     );
