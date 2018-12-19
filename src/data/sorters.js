0a5ceb68fd46db2a9ebe60d9dc1993d21d937e52
Jeremy Magland <jeremy.magland@gmail.com>
2018-12-19 16:30:19 -0500

    newest data .js files

--- src/data/sorters.js
+++ src/data/sorters.js
@@ -1,32 +1,45 @@
 const sorters = {
-  "sorters": [{
+  "sorters": [
+    {
       "name": "MountainSort4-thr3",
       "processor_name": "MountainSort4",
+      "processor_version": "4.1.0",
       "params": {
         "detect_sign": -1,
-        "adjacency_radius": 100,
+        "adjacency_radius": 50,
         "detect_threshold": 3
-      }
-    },
-    {
-      "name": "IronClust-tetrode",
-      "processor_name": "IronClust",
-      "params": {
-        "detect_sign": -1,
-        "adjacency_radius": 100,
-        "detect_threshold": 5,
-        "prm_template_name": "tetrode_template.prm"
       }
     },
     {
       "name": "SpykingCircus",
       "processor_name": "SpykingCircus",
+      "processor_version": "0.1.6",
       "params": {
         "detect_sign": -1,
-        "adjacency_radius": 100
+        "adjacency_radius": 50
+      }
+    },
+    {
+      "name": "IronClust-tetrode",
+      "processor_name": "IronClust",
+      "processor_version": "4.2.8",
+      "params": {
+        "detect_sign": -1,
+        "adjacency_radius": 50,
+        "detect_threshold": 5,
+        "prm_template_name": "tetrode_template.prm"
+      }
+    },
+    {
+      "name": "IronClust-drift",
+      "processor_name": "IronClust",
+      "processor_version": "4.2.8",
+      "params": {
+        "detect_sign": -1,
+        "adjacency_radius": 50,
+        "prm_template_name": "template_drift.prm"
       }
     }
   ]
 }
-
-export default sorters;
+export default sorters
