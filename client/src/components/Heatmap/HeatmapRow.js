41977c94278e3398c163084f28811b2c1dbc07fb
Elizabeth Lovero <elovero@flatironinstitute.org>
2019-02-25 12:24:06 -0500

    heatmap fonts.

--- client/src/components/Heatmap/HeatmapRow.js
+++ client/src/components/Heatmap/HeatmapRow.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import '../../../node_modules/react-vis/dist/style.css';
-import { XYPlot, XAxis, YAxis, HeatmapSeries, LabelSeries } from 'react-vis';
-import Preloader from '../Preloader/Preloader';
-import { isEmpty } from '../../utils';
+import React, { Component } from "react";
+import "../../../node_modules/react-vis/dist/style.css";
+import { XYPlot, XAxis, YAxis, HeatmapSeries, LabelSeries } from "react-vis";
+import Preloader from "../Preloader/Preloader";
+import { isEmpty } from "../../utils";
 
 class HeatmapRow extends Component {
   constructor(props) {
@@ -10,7 +10,7 @@ class HeatmapRow extends Component {
     this.state = { hoveredNode: null, data: null };
     this.dims = {
       height: 50,
-      width: 620,
+      width: 620
     };
     this.margin = { left: 190, right: 80, top: 5, bottom: 5 };
     if (this.props.index === 0) {
@@ -35,19 +35,19 @@ class HeatmapRow extends Component {
     let colorMap = this.props.vizDatum.map(datum => datum.color);
     colorMap.sort((a, b) => a - b);
     let withColor = this.props.vizDatum.map(datum => {
-      datum.style = colorMap.indexOf(datum.color) > 2 ? { fill: 'white' } : {};
+      datum.style = colorMap.indexOf(datum.color) > 2 ? { fill: "white" } : {};
       if (this.props.selectedStudy && this.props.selectedStudy === datum) {
-        datum.style = { fill: '#F6782D' };
+        datum.style = { fill: "#F6782D" };
       }
       return datum;
     });
     this.setState({
-      data: withColor,
+      data: withColor
     });
   }
 
   conditionalSelectStudy(datum) {
-    if (this.props.format !== 'average') {
+    if (this.props.format !== "average") {
       this.props.selectStudy(datum);
     }
   }
@@ -71,15 +71,32 @@ class HeatmapRow extends Component {
             >
               {this.props.index === 0 ? (
                 <XAxis
-                  orientation={'top'}
+                  orientation={"top"}
                   tickLabelAngle={-25}
-                  position={'start'}
+                  position={"start"}
                   title="Count above accuracy threshold"
+                  style={{
+                    text: {
+                      stroke: "none",
+                      fill: "#222",
+                      fontWeight: 600,
+                      fontSize: "12px"
+                    }
+                  }}
                 />
               ) : null}
-              <YAxis />
+              <YAxis
+                style={{
+                  text: {
+                    stroke: "none",
+                    fill: "#222",
+                    fontWeight: 600,
+                    fontSize: "12px"
+                  }
+                }}
+              />
               <HeatmapSeries
-                colorRange={['#fafafd', '#384ca2']}
+                colorRange={["#fafafd", "#384ca2"]}
                 data={data}
                 onValueMouseOver={d => {
                   this.setState({ hoveredNode: d });
@@ -96,7 +113,7 @@ class HeatmapRow extends Component {
                   this.conditionalSelectStudy(d);
                 }}
                 getLabel={d => {
-                  return d.in_range > 0 ? `${d.in_range}` : '';
+                  return d.in_range > 0 ? `${d.in_range}` : "";
                 }}
               />
             </XYPlot>
