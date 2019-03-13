af63a514657a3c5854796b9b57bb775db73263e7
Elizabeth Lovero <44589448+elovero@users.noreply.github.com>
2019-03-13 14:57:10 -0400

    Singlepage (#91)
    * New metrics.
    
    * Clean mathjax.
    
    * Remove reference to 2018 proposal.

--- client/src/components/Pages/Metrics.js
+++ client/src/components/Pages/Metrics.js
@@ -10,7 +10,8 @@ class Metrics extends Component {
     // \\hat f(\\xi)\\,e^{2 \\pi i \\xi x}
     // \\,d\\xi`;
 
-    const groundtruth = ` n_box{match}^k := # {i: |t_j^k-s_i| < \\Delta t mbox{ for some } j}`;
+    const groundtruth =
+      "n_{{ \\scriptsize match } }^k:=  i: |t_j^k-s_i| < \\Delta t  \\mbox{ for some }\\ j";
     let divStyle = {
       backgroundColor: "#fffdc0",
       borderRadius: "5px",
@@ -19,8 +20,8 @@ class Metrics extends Component {
     return (
       <div className="page__body">
         <Container className="container__heatmap">
-          <Row>
-            <Col lg={12} sm={12}>
+          <Row className="justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
               <div className="intro">
                 <p className="big">Metrics</p>
                 <div className="dividerthick" />
@@ -28,15 +29,11 @@ class Metrics extends Component {
                   Definitions of accuracy and other metrics used throughout
                   SpikeForest.
                 </p>
-                <p className="updated">
-                  Does the May 9, 2018 paper include these definitions? If so,
-                  should I link to it here?
-                </p>
               </div>
             </Col>
           </Row>
-          <Row className="container__sorter--row">
-            <Col lg={12} sm={12}>
+          <Row className="container__sorter--row justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
               <div className="card card--stats">
                 <div className="content">
                   <div className="card__label">
@@ -59,17 +56,20 @@ class Metrics extends Component {
                         <MathJax.Node inline formula={"k"} />. Let{"  "}
                         <MathJax.Node
                           inline
-                          formula={`t_j^k for j=1,\\dots,n_k`}
+                          formula={"t_j^k \\ `for` \\ j=1,\\dots,n_k"}
                         />{" "}
                         be the firing times of this unit from the sorter. Let
                         {"  "}
                         <MathJax.Node inline formula={`\\Delta t`} /> be an
                         acceptable firing time error, which we assume is shorter
-                        than half the refractory period of a true neuron. The
-                        number of matches of unit{" "}
+                        than half the refractory period of a true neuron.
+                      </p>{" "}
+                      <p>
+                        The number of matches of unit{" "}
                         <MathJax.Node inline formula={`k`} /> to the ground
-                        truth is: <MathJax.Node inline formula={groundtruth} />
+                        truth is:
                       </p>
+                      <MathJax.Node formula={groundtruth} />
                       <p>
                         Note that if more than one sorted event falls within{" "}
                         <MathJax.Node inline formula={`\\pm \\Delta t`} /> of a
@@ -77,48 +77,43 @@ class Metrics extends Component {
                         happen, by assumption.
                       </p>
                       <p>
-                        The number of missed events is{" "}
+                        The number of missed events is:{" "}
                         <MathJax.Node
-                          inline
-                          formula={`n_box{miss}^k := n_GT
-                        - n_box{match}^k.`}
+                          formula={`n_{{ \\scriptsize miss } }^k := n_{GT}
+                        - n_{{ \\scriptsize match } }^k.`}
                         />
-                        .
                       </p>
                       <p>
-                        The number of false positive events is{" "}
+                        The number of false positive events is:{" "}
                         <MathJax.Node
-                          inline
-                          formula={`n_box{fp}^k := n_k - n_box{match}^k`}
+                          formula={`n_{{ \\scriptsize fp } }^k := n_k - n_{{ \\scriptsize match } }^k`}
                         />
-                        .
+                      </p>
+                      <p style={divStyle}>
+                        Confirm citation style and see bottom section. Are other
+                        references needed on this page?
                       </p>
                       <p>
-                        To translate to the notation of Jun et al 2017,{" "}
+                        Translated to the notation of Jun et al, 2017[1] :
                         <MathJax.Node
-                          inline
-                          formula={`n_1 = n_box{miss}^k$, $n_2 = n_box{match}^k$, $n_3
-                        = n_box{fp}^k`}
+                          formula={`n_1 = n_{{ \\scriptsize miss } }^k,  \\ n_2 = n_{{ \\scriptsize match } }^k,  \\ n_3
+                        = n_{{ \\scriptsize fp } }^k`}
                         />
-                        .
                       </p>
                       <p>
-                        For code implementation, this matching of sorted events
-                        to ground-truth events uses{" "}
+                        In the code implementation, this matching of sorted
+                        events to ground-truth events uses{" "}
                         <a
                           target="_blank"
                           rel="noopener noreferrer"
-                          href="https://github.com/flatironinstitute/spikeforest/blob/dev/repos/spiketoolkit/spiketoolkit/comparison/sortingcomparison.py"
+                          href="https://github.com/flatironinstitute/spikeforest/blob/master/spikeforest/spikeforest/spiketoolkit/comparison/sortingcomparison.py"
                         >
                           a routine from spiketoolkit
                         </a>
-                        .
-                      </p>
-                      <p>
-                        The default{" "}
-                        <MathJax.Node inline formula={`\\Delta t`} />
-                        is set as <code>delta_tp</code> there, in sample units
-                        (note: the actual time thus depends on the sample rate).
+                        . In this routine, the default{" "}
+                        <MathJax.Node inline formula={`\\Delta t \\`} />
+                        is set as <code>delta_tp</code> in sample units. Note
+                        that the actual time thus depends on the sample rate.
                       </p>
                     </MathJax.Provider>
                   </div>
@@ -126,8 +121,8 @@ class Metrics extends Component {
               </div>
             </Col>
           </Row>
-          <Row className="container__sorter--row">
-            <Col lg={12} sm={12}>
+          <Row className="container__sorter--row justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
               <div className="card card--stats">
                 <div className="content">
                   <div className="card__label">
@@ -143,32 +138,36 @@ class Metrics extends Component {
                         <MathJax.Node inline formula={`k`} />, the metrics are
                         as follows, in terms of the above counts:
                       </p>
-                      <p>
-                        <strong>Precision</strong>
+                      <p className="metrics__inline">
+                        <span className="metrics__label">Precision:</span>
                         <MathJax.Node
                           inline
                           formula={
-                            "p^k := \frac{n_mbox{match}^k}{n_mbox{match}^k+n_mbox{fp}^k} = \frac{n_mbox{match}^k}{n^k} = \frac{n_2}{n_2+n_3}"
+                            "p^k := \\frac{n_{ \\scriptsize match }^k}{n_{ \\scriptsize match }^k + n_{ \\scriptsize fp }^k} = \\frac{n_{ \\scriptsize match }^k}{n^k} = \\frac{n_2}{n_2+n_3}"
                           }
                         />
                       </p>
-                      <p>Note: precision = 1 - false positive rate.</p>
                       <p>
-                        <strong>Recall</strong>
+                        <i>Precision = 1 - false positive rate.</i>
+                      </p>
+                      <p className="metrics__inline">
+                        <span className="metrics__label">Recall:</span>
                         <MathJax.Node
                           inline
                           formula={
-                            "r_k := \frac{n_mbox{match}^k}{n_mbox{miss}^k+n_mbox{match}^k} = \frac{n_mbox{match}^k}{n_{GT}} = \frac{n_2}{n_1+n_2}"
+                            "r_k := \\frac{n_{ \\scriptsize match }^k}{n_{ \\scriptsize miss }^k+n_{ \\scriptsize match }^k} = \\frac{n_{ \\scriptsize match }^k}{n_{GT}} = \\frac{n_2}{n_1+n_2}"
                           }
                         />
                       </p>
-                      <p>Note: recall = 1 - false negative rate.</p>
                       <p>
-                        <strong>Accuracy</strong>
+                        <i>Recall = 1 - false negative rate.</i>
+                      </p>
+                      <p className="metrics__inline">
+                        <span className="metrics__label">Accuracy:</span>
                         <MathJax.Node
                           inline
                           formula={
-                            " $a_k := \frac{n_mbox{match}^k}{n_mbox{miss}^k+n_mbox{match}^k+n_mbox{fp}^k} = \frac{n_mbox{match}^k}{n^k + n_{GT}-n_mbox{match}} = \frac{n_2}{n_1+n_2+n_3}$"
+                            "a_k := \\frac{n_{ \\scriptsize match }^k}{n_{ \\scriptsize miss }^k+n_{ \\scriptsize match }^k + n_{ \\scriptsize fp }^k} = \\frac{n_{ \\scriptsize match }^k}{n^k + n_{GT}-n_{ \\scriptsize match }} = \\frac{n_2}{n_1+n_2+n_3}"
                           }
                         />
                       </p>
@@ -178,8 +177,15 @@ class Metrics extends Component {
                         <MathJax.Node inline formula={"F_1"} /> score.
                       </p>
                       <p>
-                        <strong>Code</strong> see GenSortingComparisonTable in
-                        compare_with_truth.py
+                        See the GenSortingComparisonTable class in{" "}
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href="https://github.com/flatironinstitute/spikeforest/blob/master/spikeforest/spikeforest_analysis/compare_sortings_with_truth.py"
+                        >
+                          compare_sortings_with_truth.py
+                        </a>{" "}
+                        for code implementation.
                       </p>
                     </MathJax.Provider>
                   </div>
@@ -187,8 +193,8 @@ class Metrics extends Component {
               </div>
             </Col>
           </Row>
-          <Row className="container__sorter--row">
-            <Col lg={12} sm={12}>
+          <Row className="container__sorter--row justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
               <div className="card card--stats">
                 <div className="content">
                   <div className="card__label">
@@ -206,16 +212,17 @@ class Metrics extends Component {
                         <MathJax.Node inline formula={"s_i"} /> is the one with
                         the highest accuracy{" "}
                         <MathJax.Node inline formula={"a_k"} />, as defined
-                        above. Only results for this unit are reported.
+                        above.
                       </p>
                     </MathJax.Provider>
+                    <p>Only results for this unit are reported.</p>
                   </div>
                 </div>
               </div>
             </Col>
           </Row>
-          <Row className="container__sorter--row">
-            <Col lg={12} sm={12}>
+          <Row className="container__sorter--row justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
               <div className="card card--stats">
                 <div className="content">
                   <div className="card__label">
@@ -235,46 +242,43 @@ class Metrics extends Component {
                         <MathJax.Node inline formula={"m = 1, \\dots,M"} /> is
                         the channel index and{" "}
                         <MathJax.Node inline formula={"t"} /> the time point, is
-                        needed, and the list of firing times
+                        needed, and the list of firing times.
                       </p>
                       <p style={divStyle}>
                         [say something about standardized filtering]
                       </p>
                       <p>
-                        Firstly the mean waveforms{" "}
+                        Firstly, the mean waveforms{" "}
                         <MathJax.Node inline formula={"w_m^k(t)"} /> are
                         extracted for each unit label{" "}
                         <MathJax.Node inline formula={"k"} /> . Here the time
                         index is in the sample range{" "}
-                        <MathJax.Node inline formula={"1le t le T"} />.
+                        <MathJax.Node inline formula={"1 \\le t \\le T"} />.
                       </p>
                       <p>
-                        Then,{" "}
+                        Then,
                         <MathJax.Node
-                          inline
                           formula={
-                            "\\mbox{SNR} := \frac{max_{m=1,dots,M, , t=1,dots,T} |w_m^k(t)|}{sigma_m}"
+                            "{SNR}_k := \\frac{max_{m=1,\\dots,\\ M, \\ t=1,\\dots,T}  |w_m^k(t)|}{sigma_m}"
                           }
-                        />{" "}
-                        where a robust estimate of the (Gaussian-standardized)
-                        standard deviation for the{" "}
+                        />
+                        {"   "}where a robust estimate of the
+                        (Gaussian-standardized) standard deviation for the{" "}
                         <MathJax.Node inline formula={"m th"} /> channel is,{" "}
                         <MathJax.Node
-                          inline
-                          formula={"sigma_m = \frac{mbox{MAD}_m}{0.6745}"}
+                          formula={"sigma_m = \\frac{{MAD}_m}{0.6745}"}
                         />
-                        , where, for each channel{" "}
+                        where, for each channel{" "}
                         <MathJax.Node inline formula={"m"} /> , the median
                         absolute deviation of the filtered data is defined by{" "}
                         <MathJax.Node
-                          inline
-                          formula={`\\mbox{MAD} m = mbox{median}{(y_m(t) - overline{y_m})`}
+                          formula={`{MAD}_m = median(y_m(t) - \\overline{y_m})`}
                         />
-                        where{" "}
+                        where{"  "}
                         <MathJax.Node
                           inline
                           formula={
-                            "overline{y_m} := \frac{1}{T} sum_{(t = 1)}^T y_m(t)"
+                            "\\overline{y_m} := \\frac{1}{T} \\sum_{(t = 1)}^T y_m(t)"
                           }
                         />{" "}
                         is the channel average.
@@ -312,6 +316,25 @@ class Metrics extends Component {
                         </a>
                       </p>
                     </MathJax.Provider>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className="container__sorter--row justify-content-md-center">
+            <Col lg={12} sm={12} xl={10}>
+              <div className="card card--stats">
+                <div className="content">
+                  <div className="card__label">
+                    <p>
+                      <strong>References</strong>
+                    </p>
+                  </div>
+                  <div className="card__footer">
+                    <hr />
+                    [1] James Jun, Someone Else and Another Person,
+                    <i>The Name of this Article</i>, A Publication. (0) 00
+                    (2017), no. 0, 0000–0000, DOI 10.0000.
                   </div>
                 </div>
               </div>
