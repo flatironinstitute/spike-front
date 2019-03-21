945f45918d456374fd1800c026014e33e253f819
Elizabeth Lovero <elovero@flatironinstitute.org>
2019-03-21 13:35:17 -0400

    Spray stack.

--- client/src/components/ResultPairingBits/SpikeSprayV1.js
+++ client/src/components/ResultPairingBits/SpikeSprayV1.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Preloader from "../Preloader/Preloader";
 import {
+  Crosshair,
   XYPlot,
   XAxis,
-  YAxis,
   HorizontalGridLines,
   LineSeries
 } from "react-vis";
@@ -14,7 +14,7 @@ import { isEmpty } from "../../utils";
 class SpikeSprayV1 extends Component {
   constructor(props) {
     super(props);
-    this.state = { spikeCols: [] };
+    this.state = { splitSpikeCols: [], spikeCols: [], hoveredNode: null };
   }
 
   componentDidUpdate(prevProps) {
@@ -33,21 +33,17 @@ class SpikeSprayV1 extends Component {
     }, {});
   }
 
-  getMinY(data) {
-    return data.reduce((min, p) => (p.y < min ? p.y : min), data[0].y);
-  }
-
-  getMaxY(data) {
-    return data.reduce((max, p) => (p.y > max ? p.y : max), data[0].y);
-  }
-
-  // TODO: Refactor so these are consolidated
-  getMinMinY(data) {
-    return data.reduce((min, p) => (p.minY < min ? p.minY : min), data[0].minY);
-  }
-
-  getMaxMaxY(data) {
-    return data.reduce((max, p) => (p.maxY > max ? p.maxY : max), data[0].maxY);
+  addOffset(timepoints, i) {
+    let offset = -200 * i;
+    let newTPs = [];
+    timepoints.forEach(timepoint => {
+      let newtp = {
+        x: timepoint.x,
+        y: timepoint.y + offset
+      };
+      newTPs.push(newtp);
+    });
+    return newTPs;
   }
 
   buildSprayData(recDetails) {
@@ -57,7 +53,8 @@ class SpikeSprayV1 extends Component {
       console.log("Spray being made", key);
       spikeCols.push(spikeCol);
     }
-    let flatCols = [];
+    let testCols = [];
+    let splitCols = [];
     spikeCols.forEach(spikes => {
       let lines = [];
       spikes.forEach(channels => {
@@ -66,9 +63,7 @@ class SpikeSprayV1 extends Component {
           let colorLine = colorArr[i];
           lines.push({
             color: colorLine,
-            data: timepoints,
-            maxY: this.getMaxY(timepoints),
-            minY: this.getMinY(timepoints)
+            data: this.addOffset(timepoints, i)
           });
         });
       });
@@ -76,19 +71,21 @@ class SpikeSprayV1 extends Component {
       let colorArr = ["#cd3b54", "#59b953", "#ba4fb9", "gray"];
       let colorGroups = colorArr.map(color => {
         let colorGroup = lines.filter(line => line.color === color);
-        let minminY = this.getMinMinY(colorGroup);
-        let maxmaxY = this.getMaxMaxY(colorGroup);
-        let accurateOffset = Math.abs(minminY - maxmaxY) / 2 + 20;
-
         return colorGroup;
       });
-      flatCols.push(colorGroups);
+      splitCols.push(colorGroups);
+      testCols.push(lines);
     });
-
     this.setState({
-      spikeCols: flatCols
+      splitSpikeCols: splitCols,
+      spikeCols: testCols
     });
   }
+
+  // getLineColor(data) {
+  //   console.log("hoveredNide", this.state.hoveredNode);
+  //   return this.state.hoveredNode === data ? "#f6cf3f" : "#807f84";
+  // }
 
   render() {
     let loading = isEmpty(this.state.spikeCols);
@@ -109,34 +106,68 @@ class SpikeSprayV1 extends Component {
             </Card>
           </Container>
         ) : (
-          <Row>
-            {this.state.spikeCols.map((colorGroup, i) => (
-              <Col lg={3} key={`spikecol-${Math.random(i)}`}>
-                <div className="card__label">
-                  <p>
-                    <strong>{colTitles[i]}</strong>
-                  </p>
-                </div>
-                {colorGroup.map((lines, i) => (
+          <div>
+            <Row>
+              {this.state.spikeCols.map((column, i) => (
+                <Col lg={3} key={`spikecol-${Math.random(i)}`}>
+                  <div className="card__label">
+                    <p>
+                      <strong>{colTitles[i]}</strong>
+                    </p>
+                  </div>
                   <XYPlot
-                    width={300}
-                    height={300}
+                    width={350}
+                    height={350}
                     key={`spikeplot-${Math.random(i)}`}
                   >
+                    <XAxis title="Time units here" />
                     <HorizontalGridLines />
-                    {lines.map((line, i) => (
+                    {column.map((line, i) => (
                       <LineSeries
                         key={`line-${Math.random(i)}`}
-                        color={line.color}
+                        color={
+                          this.state.hoveredNode === line.data
+                            ? "#f6cf3f"
+                            : "#807f84"
+                        }
                         data={line.data}
+                        style={{ strokeWidth: 0.25 }}
+                        onNearestXY={d => this.setState({ hoveredNode: d })}
                       />
                     ))}
-                    <XAxis title={`Channel-${i + 1}`} />
                   </XYPlot>
-                ))}
-              </Col>
-            ))}
-          </Row>
+                </Col>
+              ))}
+            </Row>
+            <Row>
+              {this.state.splitSpikeCols.map((colorGroup, i) => (
+                <Col lg={3} key={`spikecol-${Math.random(i)}`}>
+                  <div className="card__label">
+                    <p>
+                      <strong>{colTitles[i]}</strong>
+                    </p>
+                  </div>
+                  {colorGroup.map((lines, i) => (
+                    <XYPlot
+                      width={300}
+                      height={300}
+                      key={`spikeplot-${Math.random(i)}`}
+                    >
+                      <HorizontalGridLines />
+                      {lines.map((line, i) => (
+                        <LineSeries
+                          key={`line-${Math.random(i)}`}
+                          color={line.color}
+                          data={line.data}
+                        />
+                      ))}
+                      <XAxis title={`Channel-${i + 1}`} />
+                    </XYPlot>
+                  ))}
+                </Col>
+              ))}
+            </Row>
+          </div>
         )}
       </div>
     );
