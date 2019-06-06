5a11997956079c0689e47149c2bd0fa490b9245d
Jeremy Magland <jeremy.magland@gmail.com>
2019-06-06 17:20:08 -0400

    update versions and date

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <p className="updated">Updated on June 3, 2019 (10am)</p>
+        <p className="updated">Updated on June 6, 2019 (5pm)</p>
         <div className="outro">
           <p className="footer__description">
             SpikeForest is a website and open source computing framework for
