a97d9dd541a150e0b9c997c6b3503e5922abcd21
Jeremy Magland <jeremy.magland@gmail.com>
2019-06-11 08:12:17 -0400

    more details on sorting result page

--- client/src/components/Pages/SortingResult.js
+++ client/src/components/Pages/SortingResult.js
@@ -68,7 +68,7 @@ class SortingResult extends Component {
                   <div className="card__footer">
                     <hr />
                     <h3>Sorting result</h3>
-                    <table className="table">
+                    <table className="table" style={{ width: 'auto' }}>
                       <thead>
                       </thead>
                       <tbody>
@@ -89,17 +89,16 @@ class SortingResult extends Component {
               <div className="card card--stats">
                 <div className="content">
                   <div className="card__footer">
-                    <table className="table">
+                    <table className="table" style={{ width: 'auto' }}>
                       <thead>
                       </thead>
                       <tbody>
-                        <tr><th>Compute time (sec)</th><td>{sortingResult.cpuTimeSec}</td></tr>
                         <tr>
                           <th>Firings output</th>
                           <td>
                             {sortingResult.firings ?
-                              (abbreviateSha1Path(sortingResult.firings + '/firings.mda', { canDownload: true })) :
-                              <span>None1</span>
+                              (abbreviateSha1Path(sortingResult.firings + '/firings.mda', { canDownload: false })) :
+                              <span>None</span>
                             }
                           </td>
                         </tr>
@@ -107,9 +106,34 @@ class SortingResult extends Component {
                           <th>Console output</th>
                           <td>
                             {sortingResult.consoleOut ?
-                              (abbreviateSha1Path(sortingResult.consoleOut + '/stdout.txt', { canDownload: true })) :
+                              (abbreviateSha1Path(sortingResult.consoleOut + '/stdout.txt', { canDownload: false })) :
                               <span>None</span>
                             }
+                          </td>
+                        </tr>
+                        <tr><th>Compute time (sec)</th><td>{sortingResult.cpuTimeSec}</td></tr>
+                        <tr>
+                          <th>Return code</th>
+                          <td>
+                            {sortingResult.returnCode}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Timed out</th>
+                          <td>
+                            {sortingResult.timedOut ? 'yes' : 'no'}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Start time</th>
+                          <td>
+                            {format_date(sortingResult.startTime)}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>End time</th>
+                          <td>
+                            {format_date(sortingResult.endTime)}
                           </td>
                         </tr>
                       </tbody>
@@ -126,9 +150,9 @@ class SortingResult extends Component {
                   <div className="card__footer">
                     <h3>Console output</h3>
                     {
-                      this.state.consoleOutExpanded ? 
+                      this.state.consoleOutExpanded ?
                         (<ConsoleOutput consoleOutPath={sortingResult.consoleOut} />) :
-                        (<button onClick={() => this.setState({consoleOutExpanded: true})}>Load console output</button>)
+                        (<button onClick={() => this.setState({ consoleOutExpanded: true })}>Load console output</button>)
                     }
                   </div>
                 </div>
@@ -142,9 +166,9 @@ class SortingResult extends Component {
                   <div className="card__footer">
                     <h3>Reproducing</h3>
                     {
-                      this.state.codeExpanded ? 
+                      this.state.codeExpanded ?
                         (<CodeForReproducing sortingResult={sortingResult} sorter={sorter} general={this.props.general} />) :
-                        (<button onClick={() => this.setState({codeExpanded: true})}>Load code for reproducing result`</button>)
+                        (<button onClick={() => this.setState({ codeExpanded: true })}>Load code for reproducing result`</button>)
                     }
                   </div>
                 </div>
@@ -155,6 +179,10 @@ class SortingResult extends Component {
       </div>
     );
   }
+}
+
+function format_date(x) {
+  return (new Date(x)).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
 }
 
 export default SortingResult;
