7d87e692e74b2472b286aa8dcb002fe0177b6af2
Elizabeth Lovero <elovero@flatironinstitute.org>
2019-08-21 13:44:09 -0400

    Remove cruft and standardize function naming on News page.

--- client/src/components/Pages/News.js
+++ client/src/components/Pages/News.js
@@ -1,60 +1,59 @@
 import React, { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-const ReactMarkdown = require('react-markdown')
+const ReactMarkdown = require("react-markdown");
 
 class News extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  render() {
+    let posts = this.props.newsPosts || [
+      {
+        title: "loading...",
+        date: "",
+        author: ""
+      }
+    ];
 
-    componentDidMount() {
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-    }
-
-    render() {
-        let posts = this.props.newsPosts || [{
-            title:'loading...',
-            date: '',
-            author: ''
-        }];
-
-        return (
-            <div className="page__body">
-                <Container className="container__heatmap">
-                    {posts.map(post => (
-                        <Row key={`${post.title}--${post.date}`} className="container__sorter--row justify-content-md-center">
-                            <Col lg={8} sm={12} xl={8}>
-                                <div className="card card--stats">
-                                    <div className="content">
-                                        <div className="card__footer">
-                                            <div style={{height: 400, overflow: 'auto'}}>
-                                                <ReactMarkdown
-                                                    source={prepare_post_markdown(post)}
-                                                    renderers={{link: LinkRenderer}}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    ))}
-                </Container>
-            </div>
-        );
-    }
+    return (
+      <div className="page__body">
+        <Container className="container__heatmap">
+          {posts.map(post => (
+            <Row
+              key={`${post.title}--${post.date}`}
+              className="container__sorter--row justify-content-md-center"
+            >
+              <Col lg={8} sm={12} xl={8}>
+                <div className="card card--stats">
+                  <div className="content">
+                    <div className="card__footer">
+                      <div style={{ height: 400, overflow: "auto" }}>
+                        <ReactMarkdown
+                          source={preparePostMarkdown(post)}
+                          renderers={{ link: renderLink }}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          ))}
+        </Container>
+      </div>
+    );
+  }
 }
 
-function LinkRenderer(props) {
-    return <a href={props.href} target="_blank">{props.children}</a>
+function renderLink(props) {
+  return (
+    <a href={props.href} rel="noopener noreferrer" target="_blank">
+      {props.children}
+    </a>
+  );
 }
 
-function prepare_post_markdown(post) {
-    return `# ${post.title}\n\n## ${post.date}\n\nby ${post.author}\n\n${post.markdown}`;
+function preparePostMarkdown(post) {
+  return `# ${post.title}\n\n## ${post.date}\n\nby ${post.author}\n\n${
+    post.markdown
+  }`;
 }
 
 export default News;
