9487f85f007ce92310a15175fb5fe6da77d6d8a3
Jeremy Magland <jeremy.magland@gmail.com>
2020-01-02 09:48:22 -0500

    support output.json in format-and-load-data.js

--- admin/bin/format-and-load-data.js
+++ admin/bin/format-and-load-data.js
@@ -2,17 +2,17 @@
 
 require("dotenv").config({ path: __dirname + "/../../.env" });
 const fs = require("fs");
-const util = require("util");
-const path = require("path");
-const fs_writeFile = util.promisify(fs.writeFile);
+// const util = require("util");
+// const path = require("path");
+// const fs_writeFile = util.promisify(fs.writeFile);
 // const fs_unlink = util.promisify(fs.unlinkSync);
-const fs_readdir = util.promisify(fs.readdir);
-const assert = require("assert");
+// const fs_readdir = util.promisify(fs.readdir);
+// const assert = require("assert");
 
 function print_usage() {
   // used below
   console.info("USAGE:");
-  console.info("./format-and-load-data.js [data_directory] [database_url]");
+  console.info("./format-and-load-data.js [data_directory_or_json_file] [database_url]");
   console.info(
     "If --database-from-env is specified, the DATABASE environment variable (from .env) will be used for the database url."
   );
@@ -25,7 +25,7 @@ function print_usage() {
 let arg1 = process.argv[2] || null;
 let arg2 = process.argv[3] || null;
 
-let data_directory = arg1;
+let data_directory_or_file = arg1;
 let database_url = arg2;
 
 if (process.argv.includes("--database-from-env")) {
@@ -36,22 +36,33 @@ if (process.argv.includes("--database-from-env-prod")) {
 }
 
 // print usage if insufficient args
-if (!data_directory || !database_url) {
+if (!data_directory_or_file || !database_url) {
   print_usage();
   process.exit(-1);
 }
 
 console.info(`USING DATABASE: ${database_url}`);
 
-// checks
-assert(
-  fs.lstatSync(data_directory).isDirectory(),
-  `Not a directory: ${data_directory}`
-);
-
-if (!fs.existsSync(data_directory + "/cleanedData")) {
-  fs.mkdirSync(data_directory + "/cleanedData");
+let data_directory = null;
+let data_file = null;
+if (fs.lstatSync(data_directory_or_file).isDirectory()) {
+  data_directory = data_directory_or_file;
 }
+else if (fs.lstatSync(data_directory_or_file).isFile()) {
+  data_file = data_directory_or_file;
+}
+else {
+  console.log(`No such file or directory: ${data_directory_or_file}`);
+  process.exit(-1);
+}
+
+// checks
+
+// if (data_directory) {
+//   if (!fs.existsSync(data_directory + "/cleanedData")) {
+//     fs.mkdirSync(data_directory + "/cleanedData");
+//   }
+// }
 
 const mongoose = require("mongoose");
 mongoose.connect(database_url, { useNewUrlParser: true });
@@ -66,30 +77,48 @@ const StudyAnalysisResult = require("../../models/StudyAnalysisResult");
 const General = require("../../models/General");
 
 // import all the raw data
-const rawSorters = JSON.parse(
-  fs.readFileSync(data_directory + "/Sorters.json", "utf-8")
-);
-const rawAlgorithms = JSON.parse(
-  fs.readFileSync(data_directory + "/Algorithms.json", "utf-8")
-);
-const rawStudySets = JSON.parse(
-  fs.readFileSync(data_directory + "/StudySets.json", "utf-8")
-);
-const rawSortingResults = JSON.parse(
-  fs.readFileSync(data_directory + "/SortingResults.json", "utf-8")
-);
-const rawStudyAnalysisResults = JSON.parse(
-  fs.readFileSync(data_directory + "/StudyAnalysisResults.json", "utf-8")
-);
-const rawGeneral = JSON.parse(
-  fs.readFileSync(data_directory + "/General.json", "utf-8")
-);
-
-async function writeNewFile(fileName, newData) {
-  let newFileName = data_directory + `/cleanedData/${fileName}.json`;
-  await fs_writeFile(newFileName, JSON.stringify(newData));
-  console.log(`Clean ${fileName}.json saved. 💾`);
+let rawSorters, rawAlgorithms, rawStudySets, rawSortingResults, rawStudyAnalysisResults, rawGeneral;
+if (data_directory) {
+  rawSorters = JSON.parse(
+    fs.readFileSync(data_directory + "/Sorters.json", "utf-8")
+  );
+  rawAlgorithms = JSON.parse(
+    fs.readFileSync(data_directory + "/Algorithms.json", "utf-8")
+  );
+  rawStudySets = JSON.parse(
+    fs.readFileSync(data_directory + "/StudySets.json", "utf-8")
+  );
+  rawSortingResults = JSON.parse(
+    fs.readFileSync(data_directory + "/SortingResults.json", "utf-8")
+  );
+  rawStudyAnalysisResults = JSON.parse(
+    fs.readFileSync(data_directory + "/StudyAnalysisResults.json", "utf-8")
+  );
+  rawGeneral = JSON.parse(
+    fs.readFileSync(data_directory + "/General.json", "utf-8")
+  );
 }
+else if (data_file) {
+  const obj = JSON.parse(
+    fs.readFileSync(data_file, "utf-8")
+  );
+  rawSorters = obj['Sorters'];
+  rawAlgorithms = obj['Algorithms'];
+  rawStudySets = obj['StudySets'];
+  rawSortingResults = obj['SortingResults'];
+  rawStudyAnalysisResults = obj['StudyAnalysisResults'];
+  rawGeneral = obj['General'];
+}
+else {
+  console.log('Unexpected error.');
+  process.exit(-1);
+}
+
+// async function writeNewFile(fileName, newData) {
+//   let newFileName = data_directory + `/cleanedData/${fileName}.json`;
+//   await fs_writeFile(newFileName, JSON.stringify(newData));
+//   console.log(`Clean ${fileName}.json saved. 💾`);
+// }
 
 async function loadIntoDB(model, data, name) {
   try {
@@ -101,62 +130,62 @@ async function loadIntoDB(model, data, name) {
   }
 }
 
-async function writeCleanData(model, name) {
-  console.info(`Writing ${name}...`);
-  const allEntries = model.find();
-  const [foundEntries] = await Promise.all([allEntries]);
-  await writeNewFile(name, foundEntries);
-  console.info(`Done writing ${name}.`);
-}
+// async function writeCleanData(model, name) {
+//   console.info(`Writing ${name}...`);
+//   const allEntries = model.find();
+//   const [foundEntries] = await Promise.all([allEntries]);
+//   await writeNewFile(name, foundEntries);
+//   console.info(`Done writing ${name}.`);
+// }
 
-function deleteFile(file) {
-  try {
-    fs.unlinkSync(file);
-  } catch (e) {
-    console.error(e);
-  }
-}
+// function deleteFile(file) {
+//   try {
+//     fs.unlinkSync(file);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
 
-async function emptyDataFolder(directory) {
-  let files = await fs_readdir(directory);
-  let filePaths = files.map(file => path.join(directory, file));
-  filePaths.forEach(file => {
-    deleteFile(file);
-  });
-}
+// async function emptyDataFolder(directory) {
+//   let files = await fs_readdir(directory);
+//   let filePaths = files.map(file => path.join(directory, file));
+//   filePaths.forEach(file => {
+//     deleteFile(file);
+//   });
+// }
 
 async function formatAndLoadData() {
   // Sorters
   await loadIntoDB(Sorter, rawSorters, "Sorters");
-  await writeCleanData(Sorter, "sorters");
+  // await writeCleanData(Sorter, "sorters");
 
   // Algorithms
   await loadIntoDB(Algorithm, rawAlgorithms, "Algorithms");
-  await writeCleanData(Algorithm, "algorithms");
+  // await writeCleanData(Algorithm, "algorithms");
 
   // Study Sets
   await loadIntoDB(StudySet, rawStudySets, "Study sets");
-  await writeCleanData(StudySet, "studySets");
+  // await writeCleanData(StudySet, "studySets");
 
 
   // Sorting Results
   await loadIntoDB(SortingResult, rawSortingResults, "Sorting results");
-  await writeCleanData(SortingResult, "sortingresults");
+  // await writeCleanData(SortingResult, "sortingresults");
 
   // Study analysis results
   await loadIntoDB(StudyAnalysisResult, rawStudyAnalysisResults, "StudyAnalysisResults");
-  await writeCleanData(StudyAnalysisResult, "studyanalysisresults");
+  // await writeCleanData(StudyAnalysisResult, "studyanalysisresults");
 
   // General
   await loadIntoDB(General, rawGeneral, "General");
-  await writeCleanData(General, "general");
+  // await writeCleanData(General, "general");
 
   // Delete WIP Files
-  await emptyDataFolder(data_directory + "/cleanedData");
-  console.log(`🗑️  All used data files in the trash.`);
+  // await emptyDataFolder(data_directory + "/cleanedData");
+  // console.log(`🗑️  All used data files in the trash.`);
 
   console.log(
-    "\n 👍👍👍👍👍👍👍👍 it's Done! \n\n 🐧 Data formatted, loaded into the DB, and deleted from the repo.🐧"
+    "\n 👍👍👍👍👍👍👍👍 it's Done! \n\n 🐧 Data formatted, loaded into the DB.🐧"
   );
   process.exit();
 }
