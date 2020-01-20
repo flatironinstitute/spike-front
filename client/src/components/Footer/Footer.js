80ad68b686b2519963a7e70d303126b630c4a7cf
lizlove <elizabethlovero@gmail.com>
2020-01-20 09:09:44 -0500

    Footer.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <p className="updated">
-          Spike-front client updated on January 7, 2020 (10:42pm)
+          Spike-front client updated on January 20, 2020 (2:12am)
         </p>
         <div className="outro">
           <p className="footer__description">
